e33fa6effa7d702e09f46f5b1a846b74ab32e076
RecLusIve-F <53204271+RecLusIve-F@users.noreply.github.com>
2020-04-01 21:52:53 +0800

    Add files via upload

--- src/com/servlet/LoginServlet.java
+++ src/com/servlet/LoginServlet.java
@@ -1,72 +1,55 @@
 package com.servlet;
 
-import com.entity.User;
-import com.service.UserService;
-import com.service.impl.UserServiceImpl;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import com.google.gson.*;
+import com.service.UserService;
+import com.service.impl.UserServiceImpl;
 
 /**
  *表示层，负责与前端进行交互，如数据的交互和请求的响应
  * @author dwaneZhou
  * @create --\
  */
-@WebServlet(urlPatterns = "/LoginServlet")//web注解，完成jsp和servlet的交互
 public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doPost(req, resp);
+    }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        String name = request.getParameter("name");
-        String password = request.getParameter("pwd");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         UserService userService = new UserServiceImpl();
-        User user = new User();
-        if (userService.login(name, password)) {
-            user.setUsername(name);
-            user.setPassword(password);
-            request.setAttribute("user", user);//存储变量信息
-            //带变量跳转，下一界面仍可获取attribute变量信息
-            request.getRequestDispatcher("login_success.jsp").forward(request, response);
+        // 设置请求的编码格式
+        req.setCharacterEncoding("UTF-8");
+        // 设置响应的编码格式
+        resp.setContentType("application/json; charset=utf-8");
+        // 获取用户输入的用户名
+        String username = req.getParameter("username");
+        // 获取用户输入的密码
+        String password = req.getParameter("password");
+        // 实例化Gson
+        Gson gson = new Gson();
+        // 判断是否登录成功
+//        if (user.equals("admin") && password.equals("admin")) {
+//            // 登录成功则输出提示信息
+//            String result = gson.toJson(new ResponseInfo(1, "登录成功"));
+//            resp.getWriter().write(result);
+//        } else {
+//            // 登录失败也输出提示信息
+//            String result = gson.toJson(new ResponseInfo(0, "用户名或密码错误"));
+//            resp.getWriter().write(result);
+//        }
+        if (userService.login(username, password)) {
+            String result = gson.toJson(new ResponseInfo(1, "登录成功"));
+            resp.getWriter().write(result);
         } else {
-            request.setAttribute("info", "登陆失败");
-            response.sendRedirect("login_failure.jsp");//重定向
+            String result = gson.toJson(new ResponseInfo(0, "用户名或密码错误"));
+            resp.getWriter().write(result);
         }
-
-        /*
-        String userName = request.getParameter("username");
-        String password = request.getParameter("password");
-        if (userName != null && !userName.equals("")) {
-            UserServiceImpl userService = new UserServiceImpl();
-            User user = new User();
-            user.setUsername(userName);
-            user.setPassword(password);
-            if (userService.findUser(userName).getPassword().equals(password))
-                request.getSession().setAttribute("user", user);
-            response.sendRedirect("index.jsp");
-
-        } else {//不存在这个用户，给出提示，转回登录页面
-            String message = "用户名或密码错误";
-            request.getSession().setAttribute("msg", message);
-            response.sendRedirect("/Student/login.jsp");
-        }
-    }
-
-
- */
-        /*
-        测试
-    public static void main(String[] args){
-
-        UserService userService = new UserServiceImpl();
-        if (userService.login("dwane","123456")){
-            System.out.println(true);
-        }
-
-         */
     }
 }
+
--- src/com/servlet/RegisterServlet.java
+++ src/com/servlet/RegisterServlet.java
@@ -1,7 +1,9 @@
 package com.servlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import com.google.gson.Gson;
+import com.service.UserService;
+import com.service.impl.UserServiceImpl;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +13,52 @@ import java.io.IOException;
  * @author dwaneZhou
  * @create --\
  */
-@WebServlet(urlPatterns = "/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doPost(req, resp);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        UserService userService = new UserServiceImpl();
+        // 设置请求的编码格式
+        req.setCharacterEncoding("UTF-8");
+        // 设置响应的编码格式
+        resp.setContentType("application/json; charset=utf-8");
+        // 获取用户输入的用户名
+        String username = req.getParameter("username");
+        // 获取用户输入的密码
+        String password = req.getParameter("password");
+        String usernameInfo = userService.checkForUsername(username);
+        String passwordInfo = userService.checkForPassword(password);
+        // 实例化Gson
+        Gson gson = new Gson();
+
+//        if (userService.login(username, password)) {
+//            String result = gson.toJson(new ResponseInfo(1, "登录成功"));
+//            resp.getWriter().write(result);
+//        } else {
+//            String result = gson.toJson(new ResponseInfo(0, "用户名或密码错误"));
+//            resp.getWriter().write(result);
+//        }
+        if(usernameInfo.equals("success") && passwordInfo.equals("success")){
+            if (userService.addUser(username, password)) {
+                String result = gson.toJson(new ResponseInfo(0, "注册成功"));
+                resp.getWriter().write(result);
+            } else {
+                String result = gson.toJson(new ResponseInfo(-1, "数据库连接失败"));
+                resp.getWriter().write(result);
+            }
+        } else if (!usernameInfo.equals("success")) {
+            String result = gson.toJson(new ResponseInfo(1, usernameInfo));
+            resp.getWriter().write(result);
+        } else {
+            String result = gson.toJson(new ResponseInfo(2, passwordInfo));
+            resp.getWriter().write(result);
+        }
     }
 }
+
+
