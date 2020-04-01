8145d22024a2449270c667e01b6f9886be304048
dwanezhou <dwanezhou@163.com>
2020-04-01 14:21:40 +0800

    1

--- src/com/service/impl/UserServiceImpl.java
+++ src/com/service/impl/UserServiceImpl.java
@@ -17,7 +17,8 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 用户名合法性检测
-     *
+     * 长度为4~8
+     * 只能包含数字、字母、下划线
      * @param username
      * @return
      */
@@ -32,7 +33,7 @@ public class UserServiceImpl implements UserService {
         if (username.length() > USERNAME_MAX_SIZE) {
             return "用户名长度不大于8";
         }
-        if (!username.matches(USERNAME)) {
+        if (username.matches(USERNAME)) {
             return "用户名包含非法字符";
         }
         if (username.equals(userDao.selectUserByName(username).getUsername())) {
@@ -43,7 +44,9 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 密码合法性检测
-     *
+     * 长度：6~16
+     * 只能包含字母、数字、下划线、！
+     * 至少包含两种类型符号
      * @param password   密码
      * @param rePassword 再次输入密码
      * @return
@@ -52,17 +55,18 @@ public class UserServiceImpl implements UserService {
     public String checkForPassword(String password, String rePassword) {
         final int PASSWORD_MIN_SIZE = 6;//密码最小长度
         final int PASSWORD_MAX_SIZE = 16;//密码最大长度
-        final String NUMBER_SMALL_LETTER = ".*[0-9a-z].*";//数字小写字母
-        final String NUMBER_CAPITAL_LETTER = ".*[0-9A-Z].*";//数字和大写字母
-        final String ALL_LETTER = ".*[A-Za-z].*";//纯字母
+        final String PASSWORD = ".*[^0-9a-zA-Z_].*";
+        final String NUMBER_LETTER ="(.*([0-9][a-zA-z]).*)|(.*([a-zA-Z][0-9]).*)";//数字小写字母
         if (password.length() < PASSWORD_MIN_SIZE) {
             return "密码长度不小于6";
         }
         if (password.length() > PASSWORD_MAX_SIZE) {
             return "密码长度不大于16";
         }
-        if (password.matches(NUMBER_SMALL_LETTER) || password.matches(NUMBER_CAPITAL_LETTER)
-                || password.matches(ALL_LETTER)) {
+        if (password.matches(PASSWORD)){
+            return "密码包含特殊字符";
+        }
+        if (!password.matches(NUMBER_LETTER)){
             return "密码过于简单";
         }
         if (!password.equals(rePassword)) {
@@ -85,6 +89,7 @@ public class UserServiceImpl implements UserService {
         }catch (RuntimeException e){
             throw new RuntimeException("异常，添加用户失败"+e);
         }
+
     }
 
     /**
@@ -102,5 +107,23 @@ public class UserServiceImpl implements UserService {
         return false;
     }
 
+
+
+    /*
+
+    测试
+
+    public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+        String username = "1234a_";
+        String password="123asdZ";
+        String rePassword="123asdZ";
+        String pwdTest = userService.checkForPassword(password,rePassword);
+        String nameTest = userService.checkForUsername(username);
+        System.out.println(nameTest);
+        System.out.println(pwdTest);
+    }
+
+     */
 
 }
